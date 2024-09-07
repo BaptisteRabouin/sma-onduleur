ad10bdd82f24ac341f3800b5b67655bdac07a695
BaptisteRabouin <baptiste.rabouin@gmail.com>
2024-09-07 20:41:36 +0200

    Create emails_config.py

---
+++ emails_config.py
@@ -0,0 +1,5 @@
+config_email = "<YOUR_EMAIL>"
+config_password = "<YOUR_PASSWORD_OR_TOKEN>"
+config_server = "<YOUR_SMTP_SERVER"
+config_server_port = <PORT_SMTP_SERVER> # For example, with gmail port can be 587
+
